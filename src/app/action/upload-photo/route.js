9233c2f6e812c7bcf91666cd3205afef8cda70d6
Ryan Rafidhea Reyhan <50486794+xzeonz@users.noreply.github.com>
2025-07-16 21:24:27 +0700

    delete action folder
    delete action folder

--- src/app/action/upload-photo/route.js
+++
@@ -1,8 +0,0 @@
-"use server";
-
-export async function uploadDummyPhoto() {
-  // Simulasi upload ke Cloudflare R2, kembalikan URL dummy
-  const url =
-    "https://www.unsulbarnews.com/wp-content/uploads/2023/11/WhatsApp-Image-2023-11-12-at-08.38.20.jpeg";
-  return { url };
-}
